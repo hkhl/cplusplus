f0887ae2fa181721cc9aabe7d9c64917b0aec739
hkhl <dhkhl235@gmail.com>
2018-04-09 00:37:11 +0800

    const_iterator

---
+++ Accelerated_C++/demo/const_iterator.cpp
@@ -0,0 +1,27 @@
+/*************************************************************************
+	> File Name: 4-9.cpp
+	> Author: wang
+	> Mail: dhkhl235@gmail.com
+	> Created Time: 2018年04月08日 星期日 21时04分14秒
+ ************************************************************************/
+
+/*方setw*/
+#include <iostream>
+#include <string>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+/*
+const_iterator 对象可以用于 const vector 或非 const vector，
+因为不能改写元素值。const 迭代器这种类型几乎没什么用处：
+一旦它被初始化后，只能用它来改写其指向的元素，但不能使它指向任何其他元素。
+*/
+/*
+1、const_iterator需要注意：这个vector本身还是可变的，只不过对const_iterator类型解引用的对象不可变。
+2、const迭代器也就是只能指向其所指向的元素，不能通过++等操作去指向其他元素。但是，所指向这个元素可以改变。
+*/
+
+    return 0;
+}
