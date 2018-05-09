03c0798304560f32a1d18e513570f2e8b8ac1264
hkhl <dhkhl235@gmail.com>
2018-05-09 23:56:48 +0800

    binary_search

--- Accelerated_C++/8.tempalte/binary_search.cpp
+++ Accelerated_C++/8.tempalte/binary_search.cpp
@@ -6,10 +6,12 @@ using namespace std;
 template <class Ran, class X>
 bool binary_search(Ran begin, Ran end, const X& x)
 {
-    while (begin < end)
+    end = end-1;
+    //while (begin < end)    //与初始化对称, 不可以用<=  如果用<= 当vector, 段错误
+    while (begin != end)
     {
         //找找区间中点
-        Ran mid = begin + (end - begin) / 2;
+        Ran mid = begin + (end - begin) / 2;  //防止溢出
 
         if (x < *mid)
             end = mid;
@@ -30,10 +32,14 @@ int main()
     v.push_back(5);
     v.push_back(9);
 
-    bool s = binary_search(v.begin(), v.end(), 0);
+    bool s = binary_search(v.begin(), v.end(), 1);
+    cout << s << endl;
+    s = binary_search(v.begin(), v.end(), 3);
+    cout << s << endl;
+    s = binary_search(v.begin(), v.end(), 5);
     cout << s << endl;
 
+    s = binary_search(v.begin(), v.end(), 9);
+    cout << s << endl;
     return 0;
 }
-
-
